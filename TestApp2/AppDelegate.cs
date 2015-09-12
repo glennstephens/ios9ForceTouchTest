eee39657e3ce9c3f64c185c5562727e0f3f25887
Glenn Stephens <glenn@orchardebusiness.com>
2015-09-13 00:37:37 +1000

    Initial Project Commit
    Some example code I’m putting together for the ForceTouch APIs for
    iOS9. Still working on the project until I get a real iPhone 6s to test
    with.

---
+++ TestApp2/AppDelegate.cs
@@ -0,0 +1,72 @@
+﻿using Foundation;
+using UIKit;
+using System.Collections.Generic;
+using CoreMotion;
+
+namespace TestApp2
+{
+	[Register ("AppDelegate")]
+	public class AppDelegate : UIApplicationDelegate
+	{
+		public override UIWindow Window {
+			get;
+			set;
+		}
+
+		public override bool FinishedLaunching (UIApplication application, NSDictionary launchOptions)
+		{
+			// Lets create some dynamic shortcut items
+			var shortcuts = new List<UIApplicationShortcutItem> ();
+
+			for (int counter = 1; counter <= 5; counter++) {
+				var item = new UIApplicationShortcutItem ("com.xamarin.university.dynamicshortcutdemo", "Item " + counter);
+				shortcuts.Add (item);
+			}
+
+			UIApplication.SharedApplication.ShortcutItems = shortcuts.ToArray ();
+
+			return true;
+		}
+
+		public override void OnResignActivation (UIApplication application)
+		{
+			// Invoked when the application is about to move from active to inactive state.
+			// This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) 
+			// or when the user quits the application and it begins the transition to the background state.
+			// Games should use this method to pause the game.
+		}
+
+		public override void DidEnterBackground (UIApplication application)
+		{
+			// Use this method to release shared resources, save user data, invalidate timers and store the application state.
+			// If your application supports background exection this method is called instead of WillTerminate when the user quits.
+		}
+
+		public override void WillEnterForeground (UIApplication application)
+		{
+			// Called as part of the transiton from background to active state.
+			// Here you can undo many of the changes made on entering the background.
+		}
+
+		public override void OnActivated (UIApplication application)
+		{
+			// Restart any tasks that were paused (or not yet started) while the application was inactive. 
+			// If the application was previously in the background, optionally refresh the user interface.
+		}
+
+		public override void WillTerminate (UIApplication application)
+		{
+			// Called when the application is about to terminate. Save data, if needed. See also DidEnterBackground.
+		}
+
+		public override void PerformActionForShortcutItem (UIApplication application, 
+			UIApplicationShortcutItem shortcutItem, 
+			UIOperationHandler completionHandler)
+		{
+			// Store the fact that we've signed in. This is where the shortcuts get their notifications.
+			// Look at the Info.plist file for more info
+		}
+	}
+}
+
+
---
+++ TestApp2/ViewController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+using UIKit;
+
+namespace TestApp2
+{
+	public partial class ViewController : UIViewController
+	{
+		public ViewController (IntPtr handle) : base (handle)
+		{
+		}
+
+		bool hasForceTouch;
+
+		public override void ViewDidLoad ()
+		{
+			base.ViewDidLoad ();
+
+			if (!UIDevice.CurrentDevice.CheckSystemVersion (9, 0))
+			{
+				hasForceTouch = false;
+			} else {
+				var traits = new UITraitCollection ();
+				hasForceTouch = traits.ForceTouchCapability == UIForceTouchCapability.Available;
+			}
+		}
+
+		public override void DidReceiveMemoryWarning ()
+		{
+			base.DidReceiveMemoryWarning ();
+			// Release any cached data, images, etc that aren't in use.
+		}
+
+		public override void TouchesBegan (Foundation.NSSet touches, UIEvent evt)
+		{
+			base.TouchesBegan (touches, evt);
+
+			if (!hasForceTouch)
+			{
+				ClickForceLabel.Text = "Clicked on a pre-iOS9 or non-ForceTouch device";
+				return;
+			}
+
+			var touch = (evt.AllTouches.AnyObject as UITouch);
+			if (touch == null)
+				return;
+			
+			var strengthOfTouch = touch.Force / touch.MaximumPossibleForce;
+			ClickForceLabel.Text = string.Format ("You have a force of {0:P}", strengthOfTouch);
+		}
+	}
+}
+
